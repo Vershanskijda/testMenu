a1691134fd4bb4b49618976b14cd1af5ab3e27e9
Vershanskijda <vershansky1990@gmail.com>
2021-02-04 14:57:59 +0300

    Second commit work

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -16,7 +16,7 @@ public class Solution extends Application{
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ScenaFXML.fxml"));
         Scene scene = new Scene(root);
-        //scene.getStylesheets().add(getClass().getResource("StyleCSS.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("StyleCSS.css").toExternalForm());
         scene.setFill(Color.LIGHTGRAY);
         stage.setResizable(false);
         stage.setScene(scene);

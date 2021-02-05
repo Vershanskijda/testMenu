628d9cc4afa972f0094d52f5bb0fb9e5369e23c6
Vershanskijda <vershansky1990@gmail.com>
2021-02-05 10:23:41 +0300

    1. Рабочая программа без шрифта Time New Romans

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,8 +6,9 @@ import java.io.File;
 
 
 public class Controller {
+
     @FXML
-    Button button1, button2;
+    Button button1, button2, button3, button4, button5, button6, button7;
 
     @FXML
     public void initialize() {
@@ -27,7 +28,7 @@ public class Controller {
     @FXML
     private void action2(ActionEvent event) {
         try {
-            File file = new File("C:\\work\\Prog\\Otrabotano\\Otrabotano.accdb");
+            File file = new File("C:\\work\\Prog\\Peredacha\\Peredacha.accdb");
             Desktop.getDesktop().open(file);
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,6 +37,46 @@ public class Controller {
 
     @FXML
     private void action3(ActionEvent event) {
+        try {
+            File file = new File("C:\\work\\Prog\\Vosvrat\\Vosvrat.accdb");
+            Desktop.getDesktop().open(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void action4(ActionEvent event) {
+        try {
+            File file = new File("C:\\work\\Prog\\Zaprosy\\Zaprosy.accdb");
+            Desktop.getDesktop().open(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void action5(ActionEvent event) {
+        try {
+            File file = new File("C:\\work\\Prog\\Sum3500\\Sum3500.accdb");
+            Desktop.getDesktop().open(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void action6(ActionEvent event) {
+        try {
+            File file = new File("C:\\work\\Prog\\Podgruska\\Podgruska.accdb");
+            Desktop.getDesktop().open(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void action7(ActionEvent event) {
         try {
             System.exit(0);
         } catch (Exception e) {
